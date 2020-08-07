0b05cc3eaf54b3cb277d648b8f8a6d9bde952971
ritchielrez <52417892+ritchielrez@users.noreply.github.com>
2020-08-07 20:05:35 +0600

    Added a square function to the main.c file.

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ void greetings(char name[])
     printf("Welcome, %s!!!", name);
 }
 
+double sqaure(double num)
+{
+    return num * num;
+}
+
 int main()
 {
     printf("Vscode is awesome!!!");
     greetings("Git");
+    sqaure(2.53);
     return 0;
 }
