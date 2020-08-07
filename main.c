01cac301084bdb53f0a4ec8a9bb45ed949f59768
ritchielrez <52417892+ritchielrez@users.noreply.github.com>
2020-08-07 12:22:02 +0600

    Added a function on main.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+void displayName(char name[])
+{
+    printf("Welcome, %s!!!", name);
+}
+
 int main()
 {
     printf("Vscode is awesome!!!");
+    displayName("Ritchiel Reza")
     return 0;
 }
