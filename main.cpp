eef2252a0d63498d73c57f94fabb6018f7a03fd0
RitchielRez <ritchielrez@gmail.com>
2020-08-09 19:27:39 +0600

    Add a string var in the "main.cpp" file.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 
+std::string expression;
 char op;
 char firstnumCh[5], secondnumCh[5];
 double firstnumI, secondnumI;
