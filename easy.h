cca88d999a2e1af235d5c018e93ad1dfdd63561b
RitchielRez <ritchielrez@gmail.com>
2020-08-10 15:17:03 +0600

    Update "easy.h" and "main.exe".

--- easy.h
+++ easy.h
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <typeinfo>
 
 #define string std::string
 
+string getStr(char * prompt)
+{
+    string str;
+
+    std::cout << prompt;
+    std::getline(std::cin, str);
+
+    return str;
+}
